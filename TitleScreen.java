b21ab47acb52152497390cc8f00fcaeba4d006c7
ikyledj <161296394+ikyledj@users.noreply.github.com>
2024-03-02 11:16:43 +0800

    Update TitleScreen.java (added modifier)

--- TitleScreen.java
+++ TitleScreen.java
@@ -2,21 +2,11 @@ import java.util.Scanner;
 
 public class TitleScreen {
     public void showTitleScreen() {
-        // ANSI escape code for yellow text
-        final String ANSI_YELLOW = "\u001B[33m";
-        // ANSI escape code to reset text formatting
-        final String ANSI_RESET = "\u001B[0m";
+        clearScreen();
         
-        // New ASCII Art for the title screen in yellow
-        System.out.println(ANSI_YELLOW + 
-            "___________.__       .___                                          ___________ \n" +
-            "\\_   _____/|  |    __| _/____   ____   _______  ____   ____  __ _. \\____    _/ \n" +
-            " |    __)_ |  |   / __ |/ __ \\ /    \\  \\_  __ \\/  _ \\ / ___\\|  |  \\ _(__    | \n" +
-            " |        \\|  |__/ /_/ \\  ___/|   |  \\  |  | \\(  <_> ) /_/  >  |  //        | \n" +
-            "/_______  /|____/\\____ |\\___  >___|  /  |__|   \\____/\\___  /|____/ \\  _______/ \n" +
-            "        \\/            \\/    \\/     \\/               /_____/         \\/ " + ANSI_RESET);
-        
-        // Welcome message and menu
+        // Call the static method from the TitleScreen class
+        printTitle();
+
         System.out.println("Welcome to Elden Rogue!");
         System.out.println("[1] START");
         System.out.println("[2] EXIT");
@@ -36,5 +26,36 @@ public class TitleScreen {
                 System.out.println("Invalid choice. Please try again.");
                 showTitleScreen();
         }
+    }
+
+    // Modifier method for printing ASCII art
+    public static void printTitle() {
+        // ANSI escape code for yellow text
+        String ANSI_YELLOW = "\u001B[33m";
+        // ANSI escape code to reset text formatting
+        String ANSI_RESET = "\u001B[0m";
+    
+        System.out.println(ANSI_YELLOW + 
+            "___________.__       .___                                          ___________ \n" +
+            "\\_   _____/|  |    __| _/____   ____   _______  ____   ____  __ _. \\____    _/ \n" +
+            " |    __)_ |  |   / __ |/ __ \\ /    \\  \\_  __ \\/  _ \\ / ___\\|  |  \\ _(__    | \n" +
+            " |        \\|  |__/ /_/ \\  ___/|   |  \\  |  | \\(  <_> ) /_/  >  |  //        | \n" +
+            "/_______  /|____/\\____ |\\___  >___|  /  |__|   \\____/\\___  /|____/ \\  _______/ \n" +
+            "        \\/            \\/    \\/     \\/               /_____/         \\/ " + ANSI_RESET);
+    }
+
+    // FOR WINDOWS (SYSTEM CLEAR SCREEN)
+    /*public static void clearScreen() { 
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }*/
+
+    // FOR MAC/UNIX/LINUX OPERATION SYSTEMS (SYSTEM CLEAR SCREEN)
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
