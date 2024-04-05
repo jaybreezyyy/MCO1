1682d455a2814a0aa41348ede48df2c84e0d949b
ikyledj <161296394+ikyledj@users.noreply.github.com>
2024-04-05 22:02:42 +0800

    Update Area2.java

--- Area2.java
+++ Area2.java
@@ -229,15 +229,14 @@ public class Area2 {
 
     private void checkSpecialTile() {
         Tile2 currentTile = getCurrentFloor()[playerX][playerY];
-        if (currentTile.isBoss()) {
-            if (!bossDefeated) {
-                System.out.println("You've encountered the boss!");
-                // Implement boss battle
+        if (currentTile.isBoss() && !bossDefeated) {
+            System.out.println("You've encountered the boss!");
+            BossBattle battle = new BossBattle(character, 2);
+            battle.display();
+            if (battle.isBossDefeated()) {
                 bossDefeated = true; // Mark the boss as defeated
                 currentTile.setBossInteracted(true); // Mark the boss tile as interacted
                 currentTile.setBoss(false); // Remove the boss tile
-            } else {
-                System.out.println("Boss already defeated. Tile has no effect.");
             }
         } else if (currentTile.isSpawn()) {
             System.out.println("You've encountered a spawn tile!");
